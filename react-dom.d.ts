150fa5acf33b09ba693a7ed9c4e57026bec0b8bb
Michael Neil Payne <thruston82@rocketmail.com>
2016-05-23 18:19:45 -0400

    unglobalizing

--- react-dom.d.ts
+++ react-dom.d.ts
@@ -2,61 +2,59 @@
 // Source: https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/b9642fb8ac07f7164dc643ddd1fa99b58ae9be8b/react/react-dom.d.ts
 import { React } from 'react';
 
-export namespace React {
-    namespace __DOM {
-        function findDOMNode<E extends Element>(instance: ReactInstance): E;
-        function findDOMNode(instance: ReactInstance): Element;
+export namespace ReactDom {
+    function findDOMNode<E extends Element>(instance: React.ReactInstance): E;
+    function findDOMNode(instance: React.ReactInstance): Element;
 
-        function render<P extends DOMAttributes, T extends Element>(
-            element: DOMElement<P, T>,
-            container: Element,
-            callback?: (element: T) => any): T;
-        function render<P>(
-            element: SFCElement<P>,
-            container: Element,
-            callback?: () => any): void;
-        function render<P, T extends Component<P, ComponentState>>(
-            element: CElement<P, T>,
-            container: Element,
-            callback?: (component: T) => any): T;
-        function render<P>(
-            element: ReactElement<P>,
-            container: Element,
-            callback?: (component?: Component<P, ComponentState> | Element) => any): Component<P, ComponentState> | Element | void;
+    function render<P extends React.DOMAttributes, T extends Element>(
+        element: React.DOMElement<P, T>,
+        container: Element,
+        callback?: (element: T) => any): T;
+    function render<P>(
+        element: React.SFCElement<P>,
+        container: Element,
+        callback?: () => any): void;
+    function render<P, T extends React.Component<P, ComponentState>>(
+        element: React.CElement<P, T>,
+        container: Element,
+        callback?: (component: T) => any): T;
+    function render<P>(
+        element: React.ReactElement<P>,
+        container: Element,
+        callback?: (component?: React.Component<P, ComponentState> | Element) => any): React.Component<P, ComponentState> | Element | void;
 
-        function unmountComponentAtNode(container: Element): boolean;
+    function unmountComponentAtNode(container: Element): boolean;
 
-        var version: string;
+    var version: string;
 
-        function unstable_batchedUpdates<A, B>(callback: (a: A, b: B) => any, a: A, b: B): void;
-        function unstable_batchedUpdates<A>(callback: (a: A) => any, a: A): void;
-        function unstable_batchedUpdates(callback: () => any): void;
+    function unstable_batchedUpdates<A, B>(callback: (a: A, b: B) => any, a: A, b: B): void;
+    function unstable_batchedUpdates<A>(callback: (a: A) => any, a: A): void;
+    function unstable_batchedUpdates(callback: () => any): void;
 
-        function unstable_renderSubtreeIntoContainer<P extends DOMAttributes, T extends Element>(
-            parentComponent: Component<any, any>,
-            element: DOMElement<P, T>,
-            container: Element,
-            callback?: (element: T) => any): T;
-        function unstable_renderSubtreeIntoContainer<P, T extends Component<P, ComponentState>>(
-            parentComponent: Component<any, any>,
-            element: CElement<P, T>,
-            container: Element,
-            callback?: (component: T) => any): T;
-        function render<P>(
-            parentComponent: Component<any, any>,
-            element: SFCElement<P>,
-            container: Element,
-            callback?: () => any): void;
-        function unstable_renderSubtreeIntoContainer<P>(
-            parentComponent: Component<any, any>,
-            element: ReactElement<P>,
-            container: Element,
-            callback?: (component?: Component<P, ComponentState> | Element) => any): Component<P, ComponentState> | Element | void;
-    }
+    function unstable_renderSubtreeIntoContainer<P extends React.DOMAttributes, T extends Element>(
+        parentComponent: React.Component<any, any>,
+        element: React.DOMElement<P, T>,
+        container: Element,
+        callback?: (element: T) => any): T;
+    function unstable_renderSubtreeIntoContainer<P, T extends React.Component<P, ComponentState>>(
+        parentComponent: React.Component<any, any>,
+        element: React.CElement<P, T>,
+        container: Element,
+        callback?: (component: T) => any): T;
+    function render<P>(
+        parentComponent: React.Component<any, any>,
+        element: React.SFCElement<P>,
+        container: Element,
+        callback?: () => any): void;
+    function unstable_renderSubtreeIntoContainer<P>(
+        parentComponent: React.Component<any, any>,
+        element: React.ReactElement<P>,
+        container: Element,
+        callback?: (component?: React.Component<P, ComponentState> | Element) => any): React.Component<P, ComponentState> | Element | void;
+}
 
-    namespace __DOMServer {
-        function renderToString(element: ReactElement<any>): string;
-        function renderToStaticMarkup(element: ReactElement<any>): string;
-        var version: string;
-    }
+export namespace ReactDomServer {
+    function renderToString(element: React.ReactElement<any>): string;
+    function renderToStaticMarkup(element: React.ReactElement<any>): string;
+    var version: string;
 }
